6f2a62cef6a72aade50ee99a6c19f4a5bc590ae7
John Allen Delos Reyes <53966880+swtmply@users.noreply.github.com>
2026-09-03 02:25:13 +0800

    feat(api): added authentication and dashboard api

--- apps/native/app/(tabs)/home.tsx
+++ apps/native/app/(tabs)/home.tsx
@@ -1,3 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { trpc } from "@/utils/trpc";
+import { ScrollView } from "react-native";
+import { Typography } from "heroui-native";
+
 export default function Home() {
+  const { data } = useQuery(trpc.dashboard.get.queryOptions());
+
   return null;
 }
